c970b778d10f9a1c3bc85bff0ea369e4f9cb526a
Karl Pauls <karlpauls@gmail.com>
2022-11-28 15:00:00 +0100

    Implement basic image dl columns (#9)

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,4 +1,33 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  [...block.children[0].children].forEach((child) => {
+    const source = child.querySelector('p > picture > source');
+    const path = new URL(source.srcset, window.location).pathname;
+    const downloadP = child.firstElementChild.nextElementSibling;
+    downloadP.remove();
+    const download = document.createElement('a');
+    download.text = downloadP.textContent;
+    download.href = path;
+    child.append(download);
+
+    function formatBytes(bytes, decimals = 2) {
+      if (!+bytes) return '0 Bytes';
+      const k = 1000;
+      const dm = decimals < 0 ? 0 : decimals;
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+      const i = Math.floor(Math.log(bytes) / Math.log(k));
+      return `${parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`;
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.responseType = 'blob';
+    xhr.onload = function () {
+      const text = document.createElement('span');
+      text.textContent = `${formatBytes(xhr.response.size)}`;
+      child.firstElementChild.append(text);
+    };
+    xhr.send();
+  });
 }
