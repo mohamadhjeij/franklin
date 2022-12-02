6d4c00b95ccd8c51697c5d0098cfbbbadcf2fba4
Karl Pauls <kpauls@adobe.com>
2022-12-02 11:13:04 +0100

    Add an article block (only hardcoded content for now) (#29)

---
+++ blocks/article-list/article-list.js
@@ -0,0 +1,170 @@
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+
+function template() {
+  return `<div class="featured-articles-with-teaser featured-articles-with-teaser--background-grey">
+    <div class="grid__container">
+        <div class="grid__structure">
+            <div class="grid__column grid__column--100">
+                <div class="featured-articles-with-teaser__inner-grid-content">
+                    <div class="featured-articles-with-teaser__headline">
+                        <div class="module-headline">
+                            <div class="grid__structure">
+                                <div class="grid__column module-headline--column">
+                                    <div class="headline hl-l headline--align-center    hl--sub-xs">
+                                        <span>
+                                            <h2>
+                                                <span class="headline__main">Weitere Artikel</span>
+                                            </h2>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        class="grid__structure featured-articles-with-teaser__items featured-articles-with-teaser__items--2">
+                        <div
+                            class="grid__column featured-articles-with-teaser__item featured-articles-with-teaser__item--article">
+                            <div class="article-teaser-item__column">
+                                <div class="article-teaser-item__content">
+                                    <a class="plain-link article-teaser-item__content-link"
+                                        href="https://www.zeiss.de/semiconductor-manufacturing-technology/news-und-events/smt-pressemeldung/mitbestimmungspreis.html"
+                                        tabindex="1">
+                                        <div class="article-teaser-item__image-wrapper">
+                                            <figure
+                                                class="lazy-image lazy-image--ratio-16_9 lazy-image--position-top article-teaser-item__image lazy-image--loaded">
+                                                <div class="lazy-image__image-container">
+                                                    <noscript>
+                                                        &lt;img class="lazy-image__image" alt="Bei der Verleihung des
+                                                        Mitbestimmungspreises: Das SMT-Betriebsratsteam mit Ministerin
+                                                        Dr. Nicole Hoffmeister-Kraut (vorne, dritte v.l.)" title="Bei
+                                                        der Verleihung des Mitbestimmungspreises: Das
+                                                        SMT-Betriebsratsteam mit Ministerin Dr. Nicole Hoffmeister-Kraut
+                                                        (vorne, dritte v.l.)"
+                                                        src="https://www.zeiss.de/content/dam/z/smt/news-events/press-photos/preisuebergabe_mitbestimmungpreis_prev.jpg/_jcr_content/renditions/original.image_file.1920.1080.0,101,1920,1181.file/preisuebergabe_mitbestimmungpreis_prev.jpg"/&gt;
+                                                    </noscript>
+                                                    <img class="lazy-image__image lazy-image__target-image "
+                                                        src="https://www.zeiss.de/content/dam/z/smt/news-events/press-photos/preisuebergabe_mitbestimmungpreis_prev.jpg/_jcr_content/renditions/original.image_file.1024.576.0,101,1920,1181.file/preisuebergabe_mitbestimmungpreis_prev.jpg"
+                                                        alt="Bei der Verleihung des Mitbestimmungspreises: Das SMT-Betriebsratsteam mit Ministerin Dr. Nicole Hoffmeister-Kraut (vorne, dritte v.l.)"
+                                                        title="Bei der Verleihung des Mitbestimmungspreises: Das SMT-Betriebsratsteam mit Ministerin Dr. Nicole Hoffmeister-Kraut (vorne, dritte v.l.)">
+                                                </div>
+                                                <div class="lazy-image__active-image-indicator"></div>
+                                            </figure>
+                                        </div>
+                                        <div class="article-teaser-item__eyebrow text--eyebrow">
+                                            <span
+                                                class="article-teaser-item__eyebrow--sub article-teaser-item__eyebrow--sub-without-main ">Nov
+                                                16, 2022</span>
+                                        </div>
+                                        <div class="headline hl-s">
+                                            <span>
+                                                <h2>
+                                                    <span class="headline__main">Betriebsrat der Carl Zeiss SMT
+                                                        GmbH gewinnt Mitbestimmungspreis 2022</span>
+                                                </h2>
+                                            </span>
+                                        </div>
+                                    </a>
+                                    <div class="article-teaser-item__meta text--caption">
+                                        <div>
+                                            Presseinformation
+                                            - <span class="article-teaser-item__meta-reading-info">2 Min.
+                                                Lesedauer</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            class="grid__column featured-articles-with-teaser__item featured-articles-with-teaser__item--article">
+                            <div class="article-teaser-item__column">
+                                <div class="article-teaser-item__content">
+                                    <a class="plain-link article-teaser-item__content-link"
+                                        href="https://www.zeiss.de/semiconductor-manufacturing-technology/news-und-events/smt-pressemeldung/zeiss-expandiert-in-wetzlar-weiteres-werk-im-dillfeld.html"
+                                        tabindex="1">
+                                        <div class="article-teaser-item__image-wrapper">
+                                            <figure
+                                                class="lazy-image lazy-image--ratio-16_9 lazy-image--position-top article-teaser-item__image lazy-image--loaded">
+                                                <div class="lazy-image__image-container">
+                                                    <noscript>
+                                                        &lt;img class="lazy-image__image" alt="Logo of Intel EPIC
+                                                        Distinguished Supplier Award 2022" title="Logo of Intel EPIC
+                                                        Distinguished Supplier Award 2022"
+                                                        src="https://www.zeiss.de/content/dam/z/smt/news-events/press-photos/visualization_zeiss_wetzlar_prev.jpg/_jcr_content/renditions/original./visualization_zeiss_wetzlar_prev.jpg"/&gt;
+                                                    </noscript>
+                                                    <img class="lazy-image__image lazy-image__target-image "
+                                                        src="https://www.zeiss.de/content/dam/z/smt/news-events/press-photos/visualization_zeiss_wetzlar_prev.jpg/_jcr_content/renditions/original.image_file.1024.576.file/visualization_zeiss_wetzlar_prev.jpg"
+                                                        alt="Logo of Intel EPIC Distinguished Supplier Award 2022"
+                                                        title="Logo of Intel EPIC Distinguished Supplier Award 2022">
+                                                </div>
+                                                <div class="lazy-image__active-image-indicator"></div>
+                                            </figure>
+                                        </div>
+                                        <div class="article-teaser-item__eyebrow text--eyebrow">
+                                            <span
+                                                class="article-teaser-item__eyebrow--sub article-teaser-item__eyebrow--sub-without-main ">Oct
+                                                12, 2022</span>
+                                        </div>
+                                        <div class="headline hl-s     ">
+                                            <span>
+                                                <h2>
+                                                    <span class="headline__main">ZEISS expandiert in Wetzlar: Weiteres
+                                                        Werk im Dillfeld</span>
+                                                </h2>
+                                            </span>
+                                        </div>
+                                    </a>
+                                    <div class="article-teaser-item__meta text--caption">
+                                        <div>
+                                            Presseinformation
+                                            - <span class="article-teaser-item__meta-reading-info">4 Min.
+                                                Lesedauer</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            class="grid__column featured-articles-with-teaser__item featured-articles-with-teaser__item--teaser">
+                            <div class="featured-articles-with-teaser__teaser">
+                                <div class="headline hl-s featured-articles-with-teaser__teaser-headline">
+                                    <span>
+                                        <h2>
+                                            <span class="headline__main">Alle Pressemeldungen</span>
+                                        </h2>
+                                    </span>
+                                </div>
+                                <div class="text text--body-m featured-articles-with-teaser__teaser-text">
+                                    <p>Finden Sie hier die neuesten Presseinformationen von ZEISS.<br>
+                                    </p>
+                                </div>
+                                <div class="featured-articles-with-teaser__teaser-link">
+                                    <div class="button-link button-link--link">
+                                        <a class=" button-link--icon  "
+                                            href="https://www.zeiss.de/corporate/newsroom/pressemitteilungen.html"
+                                            target="_blank">
+                                            <span class="button-link__content">
+                                                <span>
+                                                    <span>Zu den Pressemeldungen</span>
+                                                </span>
+                                                <span class="icon icon-external-link icon--symbol">
+                                                </span>
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid__content-bg--full featured-articles-with-teaser__grid-background"></div>
+            </div>
+        </div>
+    </div>
+</div>`;
+}
+
+export default async function decorate(block) {
+  block.innerHTML = template();
+  decorateIcons(block, true);
+}
