ac0aa2f6a3f3ddf556aa7b89a8489fe0109ebf45
Satya Deep Maheshwari <satyam@adobe.com>
2023-04-10 19:00:59 +0530

    Revert "Simplify cards markup (#82)"
    This reverts commit ec83bdf83d2138a1713f5c89dc289ac7f5e9c457.

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,23 +1,50 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-
+function template(props) {
+  return `
+  <div class="text-media-grid grid__container">
+  <div data-target-id="textMediaGrid-101653481">
+    <div class="text-media-grid__item-group lazy-image--ratio-1_1">
+      <div class="text-media-grid__items text-media-grid__items--count-${props.items.length}">
+      ${props.items.map((item) => `
+        <div class="text-media-item-vertical grid__structure text-media-item-vertical--fullwidth">
+          <div class="grid__column grid__column--100 text-media-item-vertical__media ">
+            <div class="media-component">
+              <div class="media media--image">
+                <figure data-module="LazyImage" class="lazy-image lazy-image--ratio-1_1 lazy-image--position-top lazy-image--loaded">
+                  <div class="lazy-image__image-container" data-js-select="LazyImage_image-container">
+                    <noscript>
+                    ${item.picture.querySelector('img').outerHTML}
+                    </noscript>
+                    ${item.picture.outerHTML}
+                  </div>
+                  <div class="lazy-image__active-image-indicator"></div>
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div class="grid__column text-media-item-vertical__text ">
+            <div class="text text--body-m spacing--s">
+              <p>${item.text}</p>
+            </div>
+          </div>
+        </div>
+        `)}
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+}
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
+  const items = [];
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else {
-        div.className = 'cards-card-body';
-        div.classList.add('text');
-        div.classList.add('text--body-m');
-        div.classList.add('spacing--s');
-      }
-    });
-    ul.append(li);
+    const picture = row.querySelector('picture');
+    picture.querySelector('img').classList.add('lazy-image__image');
+    const text = row.querySelector('div:last-child').textContent;
+    const item = {
+      picture,
+      text,
+    };
+    items.push(item);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
+  block.innerHTML = template({ items });
 }
