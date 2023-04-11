6434ffb1d71aff0ecc26e050f5f86b4140ce6daf
Timothee Maret <tmaret@adobe.com>
2023-04-11 14:28:56 +0200

    Setup GTM (#79)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,10 +1,32 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { sampleRUM, getMetadata } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+/**
+ * Define the environment based on the hostname
+ * @param {*} hostname
+ * @returns
+ */
+export function pageEnvironment(hostname) {
+  switch (hostname) {
+    case 'www.zeiss.de':
+    case 'www.zeiss.com':
+      return 'prod_publish';
+    case 'localhost':
+      return 'local_publish';
+    default:
+      return 'publish';
+  }
+}
+
+export function pathItem(pathname, index) {
+  // Capture the path item at the given index
+  return pathname.split('/')[index] || '';
+}
 
 // eslint-disable-next-line import/prefer-default-export
 export function getCookieConsentID(hostname) {
@@ -44,8 +66,38 @@ export function loadCookieConsent(doc, hostname) {
   }
 }
 
-loadCookieConsent(document, window.location.hostname);
+function loadGoogleTagManager(href) {
+  // Initialize the data layer
+  /* eslint no-undef: "error" */
+  const url = new URL(href);
+  const locale = getMetadata('locale');
+  const pageCountry = {
+    en: 'INT',
+    de: 'DE',
+  };
+  const conf = {
+    pageArea: 'web',
+    pageCountry: pageCountry[locale],
+    pageEnvironment: pageEnvironment(url.hostname),
+    pageIdentifier: 'main',
+    pageLanguage: locale,
+    pageLocation: 'ALL_General-Article_002',
+    pageName: url.pathname,
+    pagePool: pathItem(url.pathname, 4),
+    pageTags: '',
+    contentHierarchy1: pathItem(url.pathname, 3),
+    productName: '',
+  };
+  window.dataLayer = [conf];
+
+  // Include the manager
+  // eslint-disable-next-line
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WWQQS7V');
+}
 
 // The OneTrust website says to define this function like this.
 // eslint-disable-next-line no-unused-vars
 function OptanonWrapper() { }
+
+loadCookieConsent(document, window.location.hostname);
+loadGoogleTagManager(window.location.href);
--- test/scripts/delayed.test.js
+++ test/scripts/delayed.test.js
@@ -4,23 +4,43 @@
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
 
-let scripts;
+const PATH = '/de/semiconductor-manufacturing-technology/news-und-events/smt-pressemeldung/zeiss-trumpf-und-fraunhofer-mit-deutschem-zukunftspreis-ausgezeichnet';
+
+let delayed;
 
 document.body.innerHTML = await readFile({ path: './dummy.html' });
 document.head.innerHTML = await readFile({ path: './head.html' });
 
-describe('Delayed functionality', () => {
+describe('Delayed customer features', () => {
   before(async () => {
-    scripts = await import('../../scripts/delayed.js');
+    delayed = await import('../../scripts/delayed.js');
     document.body.innerHTML = await readFile({ path: './body.html' });
   });
 
+  it('Define Page Environment', async () => {
+    expect(delayed.pageEnvironment('unknown')).to.equal('publish');
+    expect(delayed.pageEnvironment('www.zeiss.de')).to.equal('prod_publish');
+    expect(delayed.pageEnvironment('localhost')).to.equal('local_publish');
+  });
+
+  it('Undefined Path Item', async () => {
+    expect(delayed.pathItem(PATH, 100)).to.equal('');
+  });
+
+  it('Define Content Hierarchy', async () => {
+    expect(delayed.pathItem(PATH, 3)).to.equal('news-und-events');
+  });
+
+  it('Define Page Pool', async () => {
+    expect(delayed.pathItem(PATH, 4)).to.equal('smt-pressemeldung');
+  });
+
   it('Checks the Cookie Consent ID', async () => {
-    expect(scripts.getCookieConsentID('www.zeiss.com'))
+    expect(delayed.getCookieConsentID('www.zeiss.com'))
       .to.equal('8993cba0-8683-43f1-9904-d63a3e023a9c');
-    expect(scripts.getCookieConsentID('www.zeiss.de'))
+    expect(delayed.getCookieConsentID('www.zeiss.de'))
       .to.equal('11dcda2f-5845-4860-b4c1-a3e63d9f163f');
-    expect(scripts.getCookieConsentID('localhost'))
+    expect(delayed.getCookieConsentID('localhost'))
       .to.equal('8993cba0-8683-43f1-9904-d63a3e023a9c-test');
   });
 
@@ -45,7 +65,7 @@ describe('Delayed functionality', () => {
     expect(appendedChildren.length).to.equal(0, 'Precondition');
 
     // The invocation being tested
-    scripts.loadCookieConsent(doc, 'www.zeiss.com');
+    delayed.loadCookieConsent(doc, 'www.zeiss.com');
 
     // Assertions about the behaviour
     expect(appendedChildren.length).to.equal(1);
