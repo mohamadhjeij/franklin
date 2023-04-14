ba63696829af18964645b947cb794d1dda4245a8
David Bosschaert <bosschae@adobe.com>
2023-04-14 10:03:53 +0100

    i18n of back-link text via placeholders
    Also added padding above Press Release text

--- test/blocks/hero/hero.test.js
+++ test/blocks/hero/hero.test.js
@@ -1,15 +1,42 @@
 /* eslint-disable no-unused-expressions */
 /* global describe it */
 
-import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
-
-document.body.innerHTML = await readFile({ path: '../../scripts/body.html' });
+import { addBackLink } from '../../../blocks/hero/hero.js';
 
 describe('Hero block', () => {
-  it('Builds hero block from picture and h1', async () => {
-    // await import('../../../scripts/scripts.js');
-    // expect(document.querySelector('.hero')).to.exist;
-    expect(true).to.be.true;
+  it('Adds backlink, ruler and eyebrow', () => {
+    const block = document.createElement('div');
+    const ph = {
+      pressrelease: 'PressRel',
+      newseventsbase: 'news/events',
+      backtonewsevents: 'back to overview',
+    };
+    addBackLink(block, 'de', ph, 'news/events/someevent');
+
+    const createdP = block.querySelector('.headline-brow');
+    expect(createdP.innerHTML).to.equal('PressRel');
+
+    const createdHR = block.querySelector('.back-link-line');
+    expect(createdHR.localName).to.equal('hr');
+
+    const createdLink = block.querySelector('.back-link');
+    expect(createdLink.getAttribute('href')).to.equal('/de/news/events/');
+    expect(createdLink.innerHTML).to.equal('back to overview');
+
+    const createdLinkArrow = block.querySelector('.back-link-icon');
+    expect(createdLinkArrow.getAttribute('href')).to.equal('/de/news/events/');
+    expect(createdLinkArrow.children[0].localName).to
+      .equal('svg', 'Link should contain an icon');
+  });
+
+  it('Backlink only on subpages', () => {
+    const block = document.createElement('div');
+    const ph = { newseventsbase: 'n/e/b' };
+
+    expect(block.childElementCount).to.equal(0, 'Precondition');
+    addBackLink(block, 'de', ph, '/foo/n/e/b');
+    expect(block.childElementCount).to
+      .equal(0, 'Should not add backlink on overview page');
   });
 });
